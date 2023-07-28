14fb7e378cdad1f9120e457b847e623f25490c2d
haroldCoder <haroldc2005@gmail.com>
2023-07-28 11:22:55 -0500

    middleware & implement clerk for auth

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,8 @@
 import './globals.css'
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
+import { ClerkProvider } from '@clerk/nextjs'
+import DashboardPage from './dashboard/page'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -15,8 +17,11 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
-    </html>
+    <ClerkProvider>
+      <html lang="en" className='flex flex-col-reverse'>
+        <DashboardPage/>
+        <body className={inter.className}>{children}</body>
+      </html>
+    </ClerkProvider>
   )
 }
