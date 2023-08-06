310ea6737a1753cc1b4a36e8f5267fc22c3098db
haroldCoder <haroldc2005@gmail.com>
2023-08-06 14:31:25 -0500

    use public

--- src/app/pricing/Pricing.tsx
+++ src/app/pricing/Pricing.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ButtonCheckout from '../components/ButtonCheckout';
-import { getPrices } from '@/app/api/prices/route';
+import { getPrices } from '../../../public/api/prices/route';
 
 async function Pricing() {
 
