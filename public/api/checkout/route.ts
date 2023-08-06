1168d8336378c1bb3b30893fb8bbea4d442c576c
haroldCoder <haroldc2005@gmail.com>
2023-08-06 14:23:46 -0500

    api in public folder

---
+++ public/api/checkout/route.ts
@@ -0,0 +1,13 @@
+import { ProductStripe } from "@/app/api/ProductStripe/ProductStripe";
+import { NextResponse } from "next/server";
+
+
+export const POST = async(request: Request) =>{
+    const {priceid} = await request.json()
+    
+    const ps = await new ProductStripe(process.env.STRIPE_SECRET_KEY!).createSesion('subscription', priceid)
+
+    return NextResponse.json({
+        url: ps
+    })
+}
