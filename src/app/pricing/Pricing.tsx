d4e93d292768554ba2da5464d25e26aec138176f
haroldCoder <haroldc2005@gmail.com>
2023-08-06 14:55:02 -0500

    api key as prop

--- src/app/pricing/Pricing.tsx
+++ src/app/pricing/Pricing.tsx
@@ -12,7 +12,7 @@ async function Pricing() {
                         <div className='bg-gradient-to-b text-center w-[15%] flex flex-wrap justify-between h-[30vh] flex-col from-gray-500 to-gray-900 rounded-md p-4 text-white' key={e.id}>
                             <h2>{e.nickname}</h2>
                             <h3 className='text-3xl font-bold text-gray-400'>{e.unit_amount! / 100}$</h3>
-                            <ButtonCheckout priceID={e.id} api_key_string='' />
+                            <ButtonCheckout priceID={e.id} api_key_string={process.env.STRIPE_SECRET_KEY!} />
                         </div>
                     ))
                 }
